11d5e805f7ce641191eb13dc44794cffb7e88bc8
nngvn8 <smartin-online@web.de>
2026-03-01 21:00:05 +0100

    feat: ensure correct column setup integrated into pipeline of client

--- cpp/code/src/client/DBClient.cpp
+++ cpp/code/src/client/DBClient.cpp
@@ -275,6 +275,7 @@ void DBClient::runOptimizerPipeline(ASTNode* root) {
 
     // Generate IR tree for second optimizer
     std::shared_ptr<PlanNode> ir_root = astToIr(root);
+    ensureCorrectColumnSetup(ir_root);
     createPlanDotFile(*ir_root, "ir_plan.dot", DotContentType::IR_DATA);
 
     // Place semi joins
